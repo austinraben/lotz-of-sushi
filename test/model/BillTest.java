7041d9073c4bdbcea90805cffe012fb814a9e0f4
ColinGale <colingale1201@gmail.com>
2025-04-16 15:56:48 -0700

    Commit to merge

--- test/model/BillTest.java
+++ test/model/BillTest.java
@@ -19,5 +19,19 @@ class BillTest {
 		myRestaurant.closeOrder(myTable, 1, 2.50);
 		assertEquals(myBill.getPriceBeforeTip(), 8.99);
 	}
+	
+	@Test
+	void testTipPrice() {
+		Restaurant myRestaurant = new Restaurant("Bill Test");
+		myRestaurant.seatCustomers(1, 1);
+		Table myTable = myRestaurant.getTableByNumber(1);
+		myRestaurant.assignServerToTable("Colin Gale", myTable);
+		myRestaurant.orderItem(myTable, 1, "Spicy Chicken Ramen", "None");
+
+		
+		Bill myBill = myRestaurant.getBillByTable(myTable);
+		myBill.updateTipPrice(5.99);
+		assertEquals(myBill.getPriceAfterTip(), 14.98);
+	}
 
 }
