a1453f6905eec7824e642cfc56558d2f0852e1c4
ColinGale <colingale1201@gmail.com>
2025-04-24 20:45:37 -0700

    Added comments, fixed escaping reference, small changes to UI

--- src/model/Bill.java
+++ src/model/Bill.java
@@ -1,10 +1,16 @@
+/*
+ * This class models a Bill, storing a price before and after the tip is added. Each Bill is associated with an Order
+ * which is associated with each customer as well. There is also a Bill associated with the table, representing the combination
+ * of all orders at the table.
+ */
+
 package model;
 
 public class Bill {
 	private double priceBeforeTip;
 	private double priceAfterTip;
 
-	
+	// default constructor
     public Bill() {
         this.priceBeforeTip = 0;
         this.priceAfterTip = 0;
@@ -17,11 +23,19 @@ public class Bill {
     }
     
     public void updateBeforeTipPrice(double price) {
+    	/*
+    	 * This method updates the price before tip and after tip so the two variables are consistent with each other
+    	 * until the tip is added.
+    	 */
     	priceBeforeTip += price;
     	priceAfterTip += price;
     }
     
     public void changeBeforeTipPrice(double newPrice) {
+    	/*
+    	 * This method sets the price before tip and the respective price after tip so the two variables are consistent
+    	 * with each other.
+    	 */
     	priceAfterTip += newPrice - priceBeforeTip;
     	priceBeforeTip = newPrice;
     	
@@ -32,11 +46,17 @@ public class Bill {
     }
     
     public double getPriceBeforeTip() {
+    	/*
+    	 * Gets the price before tip rounded to two decimal places (representing dollars and cents)
+    	 */
     	String roundedPriceBeforeTip = String.format("%.2f", priceBeforeTip);
     	return Double.parseDouble(roundedPriceBeforeTip);
     }
     
     public double getPriceAfterTip() {
+    	/*
+    	 * Gets the price after tip rounded to two decimal places (representing dollars and cents)
+    	 */
     	String roundedPriceAfterTip = String.format("%.2f", priceAfterTip);
     	return Double.parseDouble(roundedPriceAfterTip);    }
 
--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,3 +1,8 @@
+/*
+ * This class models a Customer, storing an order taken by a server and which table the customer is assigned to. This class has
+ * methods to order an item, tip, and change their Bill total (associated with the order).
+ */
+
 package model;
 
 import java.util.Objects;
@@ -6,12 +11,17 @@ public class Customer {
     private Order order;
     private Table assignedTable;
     
+    // default constructor
     public Customer(Table table, int orderNum, String serverName) {
     	assignedTable = table;
         this.order = new Order(orderNum, serverName);
     }
     
     public void orderItem(MenuItem item, String modification, Menu menu) {
+    	/*
+    	 * This method orders a MenuItem from a specific Menu, which adds a OrderedItem
+    	 * object to the order's internal list.
+    	 */
         order.orderItem(item.getItemName(), modification, menu, item);
     }
     
@@ -40,6 +50,8 @@ public class Customer {
     public Order getOrder() {
     	return new Order(order);
     }
+    
+    // hash code and equals
     
     @Override
     public int hashCode() {
